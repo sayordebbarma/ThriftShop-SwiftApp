b266c9b000a9cb71f03a4b816d7aa3c20188c099
sayordebbarma <sayor.debbarma2021@vitbhopal.ac.in>
2023-03-12 19:03:07 +0530

    Initial Commit

---
+++ ThriftShop/ThriftShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThriftShopApp.swift
+//  ThriftShop
+//
+//  Created by Sayor Debbarma on 12/03/23.
+//
+
+import SwiftUI
+
+@main
+struct ThriftShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
